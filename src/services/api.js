0b2c19dcbff89b4b911bb01dd3679b86666c01ce
rodrigoaugusto99 <rodrigoaugusto51@outlook.com>
2024-02-26 20:49:26 -0300

    add: cors

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-//aqui fica o endereco do backend, pra nao precisar repetir em todas as requisicoes
-export const api = axios.create({
-    baseURL: 'http://localhost:3333'
-})
-//ai quando for usar a api:
-api.get('/users/:id')
